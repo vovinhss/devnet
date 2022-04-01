bb72c98598b6c05c8496320275115ff9d613837d
vovinhss <vovinhss@gmail.com>
2022-04-01 16:34:41 +0700

    changing

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 from email import header
+import re
+from matplotlib.style import use
 import requests
 import json
 
@@ -8,6 +10,10 @@ data = {}
 
 response = requests.get(url,headers=headers, data=data)
 print(type(response))
-response = response.json()
+response = response.json() #convert to dict type format
 print(type(response))
-#print(json.dumps(response, indent=4))
+print(json.dumps(response, indent=4))
+users = response['support']['url']
+print(response['data'][0]['name'])
+print(type(response['support']))
+print(users)
